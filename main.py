5cfef8db4fbcedd0459e834a53d7e80b1c38f616
mxs <mxs@dismail.de>
2025-10-27 02:02:17 +0100

    we were so close

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 import trio
 import pyrstest
 
+print(pyrstest.__dir__())
 print(pyrstest.sum_as_string(1, 2))
 
 
 async def main():
     while True:
         print("waiting")
-        await trio.sleep(1)
+        # await trio.sleep(1)
+        await pyrstest.sleep(1)
 
 
 if __name__ == "__main__":
--- src/lib.rs
+++ src/lib.rs
@@ -4,14 +4,33 @@ mod pyfuture;
 mod trio;
 
 /// Formats the sum of two numbers as string.
-#[pyfunction]
-fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
-    Ok((a + b).to_string())
-}
+//#[pyfunction]
+//fn sum_as_string(a: usize, b: usize) -> PyResult<String> {
+//    Ok((a + b).to_string())
+//}
 
 /// A Python module implemented in Rust.
 #[pymodule]
 fn pyrstest(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    //  m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
+    m.add_function(wrap_pyfunction!(async_sleep, m)?)?;
     Ok(())
+}
+
+fn tokio() -> &'static tokio::runtime::Runtime {
+    use std::sync::OnceLock;
+    static RT: OnceLock<tokio::runtime::Runtime> = OnceLock::new();
+    RT.get_or_init(|| tokio::runtime::Runtime::new().unwrap())
+}
+
+async fn sleep(seconds: u64) -> Result<(), PyErr> {
+    let sleep = async move { tokio::time::sleep(std::time::Duration::from_secs(seconds)).await };
+    tokio().spawn(sleep).await.unwrap();
+    Ok(())
+}
+
+#[pyfunction]
+#[pyo3(name = "sleep")]
+fn async_sleep(seconds: u64) -> trio::Coroutine {
+    trio::Coroutine::from_future(sleep(seconds))
 }
