c97e69f68da7e7fb634cacc823408030250ed6f0
mxs <mxs@dismail.de>
2025-11-09 13:13:35 +0100

    switch to declarative pyo3 modules

--- src/lib.rs
+++ src/lib.rs
@@ -4,25 +4,25 @@ mod pyfuture;
 mod trio;
 
 #[pymodule]
-fn pyrstest(m: &Bound<'_, PyModule>) -> PyResult<()> {
-    m.add_function(wrap_pyfunction!(async_sleep, m)?)?;
-    Ok(())
-}
+mod pyrstest {
+    use super::*;
 
-fn tokio() -> &'static tokio::runtime::Runtime {
-    use std::sync::OnceLock;
-    static RT: OnceLock<tokio::runtime::Runtime> = OnceLock::new();
-    RT.get_or_init(|| tokio::runtime::Runtime::new().unwrap())
-}
+    #[pyfunction]
+    #[pyo3(name = "sleep")]
+    fn async_sleep(seconds: u64) -> trio::Coroutine {
+        trio::Coroutine::from_future(sleep(seconds))
+    }
 
-async fn sleep(seconds: u64) -> Result<(), PyErr> {
-    let sleep = async move { tokio::time::sleep(std::time::Duration::from_secs(seconds)).await };
-    tokio().spawn(sleep).await.unwrap();
-    Ok(())
-}
+    fn tokio() -> &'static tokio::runtime::Runtime {
+        use std::sync::OnceLock;
+        static RT: OnceLock<tokio::runtime::Runtime> = OnceLock::new();
+        RT.get_or_init(|| tokio::runtime::Runtime::new().unwrap())
+    }
 
-#[pyfunction]
-#[pyo3(name = "sleep")]
-fn async_sleep(seconds: u64) -> trio::Coroutine {
-    trio::Coroutine::from_future(sleep(seconds))
+    async fn sleep(seconds: u64) -> Result<(), PyErr> {
+        let sleep =
+            async move { tokio::time::sleep(std::time::Duration::from_secs(seconds)).await };
+        tokio().spawn(sleep).await.unwrap();
+        Ok(())
+    }
 }
